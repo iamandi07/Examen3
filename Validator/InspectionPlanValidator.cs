101a0fabeaa50de0415e84b94e065769fe80527a
Szabi <szabiadam@yahoo.com>
2020-07-04 09:13:04 +0300

    Validator
    Validator

---
+++ Validator/InspectionPlanValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen3.Model;
+using FluentValidation;
+
+namespace Examen3.Validator
+{
+    public class InspectionValidator : AbstractValidator<InspectionPlan>
+    {
+        public InspectionPlanValidator()
+        {
+            RuleFor(x => x.Temperature).GreaterThanOrEqualTo(18).LessThanOrEqualTo(28);
+            RuleFor(x => x.Humidity).GreaterThanOrEqualTo(25).LessThanOrEqualTo(75);
+            RuleFor(x => x.Pressure).GreaterThanOrEqualTo(936).LessThanOrEqualTo(1144);
+        }
+        
+    }
+}
---
+++ Validator/PersonValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen3.Model;
+using FluentValidation;
+
+namespace Examen3.Validator
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.PhoneNumber).Length(10);
+        }
+    }
+}
